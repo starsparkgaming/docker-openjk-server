fdcf1d4ee4f79be20fa0c64c27f54c04f29c2b60
starsparkgaming <79425116+starsparkgaming@users.noreply.github.com>
2021-03-05 01:50:07 -0500

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,7 +8,7 @@ tar -xzvf steamcmd.tar.gz -C /home/container/steamcmd
 rm /home/container/steamcmd.tar.gz
 
 # install steamm app with steamcmd
-./steamcmd/steamcmd.sh +login "${STEAM_USERNAME}" "${STEAM_PASSWORD}" "${STEAM_AUTH}" @sSteamCmdForcePlatformType "windows" +force_install_dir /home/container/app +app_update 6020 +quit
+./steamcmd/steamcmd.sh +login "${STEAM_USERNAME}" "${STEAM_PASSWORD}" "${STEAM_AUTH}" +@sSteamCmdForcePlatformType "windows" +force_install_dir /home/container/app +app_update 6020 +quit
 
 # download and install OpenJK
 curl -SL -o openjk.tar.gz "https://builds.openjk.org/openjk-2018-02-26-e3f22070-linux-64.tar.gz"

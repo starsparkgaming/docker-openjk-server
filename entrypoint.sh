26d3bd4bde1e059a1fcc0868f26b2ea7fa1ee3c3
starsparkgaming <79425116+starsparkgaming@users.noreply.github.com>
2021-03-05 02:02:08 -0500

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -12,7 +12,9 @@ rm /home/container/steamcmd.tar.gz
 
 # download and install OpenJK
 curl -SL -o openjk.tar.gz "https://builds.openjk.org/openjk-2018-02-26-e3f22070-linux-64.tar.gz"
-tar -xzvf openjk.tar.gz -C "/home/container/app/GameData"
+tar -xzvf openjk.tar.gz -C "/home/container"
+mv /home/container/install/JediAcademy /home/container/app/GameData
+rm -r /home/container/install
 rm /home/container/openjk.tar.gz
 
 # run the Server
